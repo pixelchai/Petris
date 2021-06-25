74571884925c130b263fd0fc07d7ddbf67120a60
pixelzery <pixelzery@gmail.com>
2021-06-25 17:33:17 +0100

    project setup

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    context: path.join(__dirname, "src"),
+    entry: ["./js/main.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "game.min.[hash:8].js",
+    },
+    target: "web",
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([{ from: "res/", to: "res/" }], {
+            ignore: [],
+            debug: "debug",
+            copyUnmodified: true,
+        }),
+        new HtmlPlugin({
+            file: path.join(__dirname, "dist", "index.html"),
+            template: "./index.html",
+        }),
+    ],
+};
